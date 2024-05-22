245c33dee4c7e7479d506a0b96f4012d721acddb
effiecancode <faitheffie25@gmail.com>
2024-05-22 23:24:53 +0300

    Implemented: Authentication: register, login & logout

---
+++ mediSched/authentication/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from . import views
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='authentication/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='authentication/login'), name='logout'),
+    path('register/', views.register, name='register'),
+]
--- mediSched/mediSched/settings.py
+++ mediSched/mediSched/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'appointments',
+    'authentication',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
